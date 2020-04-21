733eb5a4d9833e86cc8a5c7251282624a084f090
KMT 184.05 <oncwnuDpNzQMO7Ay9Yg5TJoyK7Uk@git.weixin.qq.com>
2020-04-21 08:03:12 +0800

    Initial commit

---
+++ LocationChange/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocationChange
+//
+//  Created by Jiangkun L. on 2020/4/20.
+//  Copyright © 2020 Jiangkun L. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Location Change")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
